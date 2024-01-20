837a121e8b635f83e4091e48bc38489cb854b5ee
Zachary L. Cain <zacharylcain@gmail.com>
2024-01-20 13:46:59 -0600

    changes for readline to work

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,11 +12,11 @@ let candidateAnswer = "";
 
 //TODO: Variables for Part 2
 let questions = [ 
-"Who was the first American woman in space?" ,
-"True or false: 5 kilometer == 5000 meters?" ,
-"(5 + 3)/2 * 10) = ?" ,
-"Given the array 8, Orbit,Trajectory, 45 what entry is at index 2?" ,
-"What is the minimum crew size for the ISS?"
+"Who was the first American woman in space? ",
+"True or false: 5 kilometer == 5000 meters? ",
+"(5 + 3)/2 * 10 = ? ",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? "
 ]
 
 let correctAnswers = [
@@ -40,6 +40,7 @@ function askQuestion() {
   // ask the candidate questions 1.2b //
 for (let i = 0; i < questions.length; i++) ;
 
+candidateAnswers = input.question(questions);
 }
 
 function gradeQuiz() {
