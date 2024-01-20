7e5627bebbee6f9ddc340dda19dc727cbfa30f9d
Zachary L. Cain <zacharylcain@gmail.com>
2024-01-19 20:10:36 -0600

    changes to test pass 8

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,19 +12,18 @@ let candidateAnswer = "";
 
 //TODO: Variables for Part 2
 let questions = [ 
-{question:'Who was the first American woman in space?'},
-{question:'True or false: 5 kilometer == 5000 meters?'},
-{question:'(5 + 3)/2 * 10,)= ?'},
-{question:'Given the array [8, Orbit,Trajectory, 45] what entry is at index 2?'},
-{question:'What is the minimum crew size for the ISS?'}
+['Who was the first American woman in space?'],
+['True or false: 5 kilometer == 5000 meters?'],
+['(5 + 3)/2 * 10,)= ?'],
+['Given the array [8, Orbit,Trajectory, 45] what entry is at index 2?'],
+['What is the minimum crew size for the ISS?']
 ];
 let correctAnswers = [
-  {answer: 'Sally Ride'},
-  {answer: 'true'},
-  {answer: 40},
-  {answer: 'Trajectory'},
-  {answer: 3}
-];
+['Sally Ride'],
+['true'],
+[40],
+['Trajectory'],
+[3]];
 
 
 let candidateAnswers = [];
@@ -35,17 +34,16 @@ function askForName() {
 candidateName = input.question("Please enter your name: "); 
 }
 
-function askQuestion() {
-// TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);}
+function askQuestion()  {
+  // ask the candidate questions 1.2b //
 for (let i = 0; i < questions.length; i++) {
-    const response = askQuestion(questions[i]);
-    userResponses.push(response);
+const candidateAnswer = askQuestion(questions[i]);
+candidateAnswer.push(candidateAnswers);
 }
-
+}
 function gradeQuiz() {
 // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //  
-for (let i = 0; i < questions.length; i++) {
+ for (let i = 0; i < questions.length; i++) {
   console.log(`Question: ${questions[i]}`);
   console.log(`Your Answer: ${userResponses[i]}`);
   console.log(`Correct Answer: ${correctAnswers[i]}\n`);
